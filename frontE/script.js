2d735c682e2e233f31de395e423af6637c8dfcdc
Poornima.N <97184487+Poornima-nagesh@users.noreply.github.com>
2022-09-18 21:11:20 +0530

    Add files via upload

---
+++ frontE/script.js
@@ -0,0 +1,16 @@
+var fname='poornima';
+console.log(name)
+
+var a=10;
+var b=20;
+console.log(a+b)
+var lname="p";
+console.log(fname+'  '+lname)
+function add(x,y)
+{
+    
+    console.log(x+y)
+}
+add(10,40);
+
+
